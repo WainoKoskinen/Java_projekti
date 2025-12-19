70b93d98cbc8d3f6efa5c7dba70b852860bf0461
e2402230 <e2402230@edu.vamk.fi>
2025-12-19 11:31:14 +0000

    input kenttiä

--- Lahjalista.java
+++ Lahjalista.java
@@ -1,37 +1,88 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-public class Lahjalista{
+public class Lahjalista {
 
-    public static void main(String[] args){
-        JFrame frame =new JFrame("Lahjalista");
-        frame.setSize(400,300);
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gift list");
+
+        frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
 
-        JButton button =new JButton("klikkaa");
-        frame.add(button);
+    
+        JPanel ItemInput = new JPanel();
+        ItemInput.setLayout(new GridLayout(2, 2)); 
 
-        JLabel label= new JLabel("teksti");
-        frame.add(label);
+       
+        JTextField tuote = new JTextField();
+        JTextField hintaKentta = new JTextField();
+        JTextField Username = new JTextField();
+        JTextField kategoria = new JTextField();
 
-        button.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent e){
-                    label.setText("painoit");
-                }
-            }
-        );
+        //input boxes down
+        ItemInput.add(new JLabel("Price (€):"));
+        ItemInput.add(new JLabel("Gift name:"));
+        ItemInput.add(new JLabel("Saaja:"));
+        ItemInput.add(new JLabel("kategoria:"));
+        ItemInput.add(tuote);
+        ItemInput.add(hintaKentta);
+        ItemInput.add(Username);
+        ItemInput.add(kategoria);
+
+       
         
+        JPanel nappiPaneeli = new JPanel(new FlowLayout());
+        nappiPaneeli.add(new JButton("Save"));
+        nappiPaneeli.add(new JButton("Load"));
 
+    
+        JPanel under = new JPanel();
+        under.setLayout(new BorderLayout());
 
+        under.add(ItemInput, BorderLayout.CENTER); 
+        under.add(nappiPaneeli, BorderLayout.SOUTH); 
 
-        frame.setLayout(new FlowLayout());
+     
+        frame.add(under, BorderLayout.SOUTH);
+
+        
         frame.setVisible(true);
+    }
+
+        public class lahja{
+
+     //attributes
+        private double Price;
+        private String Gift;
+        private String receiver;
+        private String gatecory;
+        //constructor
+        public lahja(double Price, String Gift, String receiver, String gatecory){
+            this.Price =Price;
+            this.Gift =Gift;
+            this.receiver = receiver;
+            this.gatecory = gatecory;
+        }
+
+        public double getPrice(){
+            return Price;
+        }
+        public String getGift(){
+            return Gift;
+        }
+        public String getreceiver(){
+            return receiver;
+        }
+        public String getgatecory(){
+            return gatecory;
+        }
 
     }
 }
